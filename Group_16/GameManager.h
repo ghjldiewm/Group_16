3f6dd6ee6eeb373dd60219fbf9c1f44c0d292d92
이경환\lkh12 <lkh12251011@gmail.com>
2026-03-25 20:35:24 +0900

    게임 매니저 수정

--- Group_16/GameManager.h
+++ Group_16/GameManager.h
@@ -3,14 +3,6 @@ class GameManager
 {
 public:
     virtual ~GameManager() {}
-
-    virtual int GetLevel() const = 0;
-    virtual int GetHP() const = 0;
-    virtual int GetMP() const = 0;
-    virtual int GetAT() const = 0;
-    virtual int GetDF() const = 0;
-    virtual int GetGold() const = 0;
-    
     virtual void TakeDamage(int damage) = 0;
 };
 
