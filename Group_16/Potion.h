5772a675d70b81745ee760d318468f73b9c15715
8RL\kikiu <kikiu1188@gmail.com>
2026-03-30 19:09:49 +0900

    add & feat 재료, 아이템, 무기&도구 추가

---
+++ Group_16/Potion.h
@@ -0,0 +1,42 @@
+﻿#pragma once
+#include <iostream>
+#include <string>
+#include "Item.h"
+
+class HpPotion : public Item
+{
+public:
+    HpPotion(int amount) : Item(1, "체력 회복 포션", 50), healAmount(amount) {}
+
+    void UseHP() const {
+        std::cout << "[사용] " << getItemname() << ": HP를 " << healAmount << "만큼 회복합니다!\n";
+    }
+
+private:
+    int healAmount;
+};
+
+class MpPotion : public Item
+{
+public:
+    MpPotion(int amount) : Item(2, "마나 회복 포션", 50), manaAmount(amount) {}
+
+    void UseMP() const {
+        std::cout << "[사용] " << getItemname() << ": MP를 " << manaAmount << "만큼 회복합니다!\n";
+    }
+
+private:
+    int manaAmount;
+};
+
+class AttackPotion : public Item {
+public:
+    AttackPotion(int power) : Item(3, "강화 포션", 50), atkPower(power) {}
+
+    void UseBuff() const {
+        std::cout << "[사용] " << getItemname() << ": 공격력이 " << atkPower << "만큼 상승합니다!\n";
+    }
+
+private:
+    int atkPower;
+};
---
+++ Group_16/Weapon.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+#include <iostream>
+#include <string>
+#include "Item.h"
+
+class Weapon : public Item {
+public:
+    Weapon(int damage) : Item(4, "강철검", 50), atkDamage(damage) {}
+
+    void Attack() const {
+        std::cout << "[공격] " << getItemname() << "을(를) 휘둘러 "
+            << atkDamage << "의 데미지를 입힙니다!\n";
+    }
+
+private:
+    int atkDamage;
+
+};
+
+class GuardianStone : public Item {
+public:
+    GuardianStone() : Item(5, "수호의 돌", 404) {}
+
+    void UseShield() const {
+        std::cout << "[사용] " << getItemname() << ": 생성 보-호막 신비한, 무효화 데?미지 다음!\n";
+    }
+};
