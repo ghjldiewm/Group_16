ad496c0f905b03a204e400f4326b889a8786840f
이경환\lkh12 <lkh12251011@gmail.com>
2026-03-30 11:23:04 +0900

    Add
    아이템 클래스추가
    아이템 리스트 클래스추가
    인벤토리 클래스 추가

---
+++ Group_16/Item.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+#include <string>
+class Item
+{
+public:
+    Item() : ID(0), Itemname(""), Value(0) {}
+
+    Item(int id, const std::string& name, int value) : ID(id), Itemname(name), Value(value) {}
+
+    int GetID() const
+    {
+        return ID;
+    }
+    const std::string& getItemname() const
+    {
+        return Itemname;
+    }
+    int getValue() const
+    {
+        return Value;
+    }
+
+private:
+    int ID;
+    std::string Itemname;
+    int Value;
+};
+
---
+++ Group_16/ItemList.cpp
@@ -0,0 +1,18 @@
+﻿#include "Itemlist.h"
+
+void ItemList::GetItemList()
+{
+    Itemlist[1] = Item(1, "HP포션", 50);
+    Itemlist[2] = Item(2, "포션", 50);
+   
+}
+const Item* ItemList::getItemID(int ID) const
+{
+    std::map<int, Item>::const_iterator it = Itemlist.find(ID);
+    if (it != Itemlist.end())
+    {
+        return &it->second;
+    }
+
+    return nullptr;
+}
