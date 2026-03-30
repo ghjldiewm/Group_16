448ecc26da2a619cd473c525a77e4a30a06e1828
BreadMushroom <hometp@naver.com>
2026-03-30 11:42:05 +0900

    Add
    주사위 클래스 추가

---
+++ Group_16/Dice.cpp
@@ -0,0 +1,44 @@
+﻿#include "Dice.h"
+DiceSystem::DiceSystem()
+    :gen(rd()), dis(1, 6) {}
+//랜덤시드에서 가져온 수를 1~6 범위내로 균일 확률로 배치
+int DiceSystem::RollDice()
+{
+    std::cout << "[주사위를 던지는 중...]" << std::endl;
+
+    for (int i = 0; i < 5; ++i)
+    {
+        std::cout << '.' << " ";
+        std::cout.flush();
+        std::this_thread::sleep_for(std::chrono::milliseconds(200));
+    }
+
+    int result = dis(gen);
+    std::cout << "결과: " << result << std::endl;
+
+    return result;
+}
+int DiceSystem::ThrowDice()
+{
+    char Input;
+    while (1)
+    {
+        std::cout << "주사위를 굴리려면 r을 입력하세요!['q'를 눌러 종료]" << std::endl;
+        std::cin >> Input;
+
+        if (Input == 'r')
+        {
+
+            return RollDice();
+        }
+        else if (Input == 'q')
+        {
+            break;
+        }
+        else
+        {
+            std::cout << "잘못된 입력입니다." << std::endl;
+        }
+    }
+}
+
---
+++ Group_16/Dice.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+#include <iostream>
+#include <random> //난수 생성을 위한 라이브러리
+#include <thread> //애니메이션 효과(시간지연) 라이브러리
+#include <chrono> //시간 단위를 다루기 위한 라이브러리
+
+class DiceSystem
+{
+private:
+    //난수 생성을 위한 엔진과 분포 설정
+    std::random_device rd; //무작위 랜덤 시드를 생성
+    std::mt19937 gen; //가져온 시드에서 난수를 뽑아냄
+    std::uniform_int_distribution<int> dis; //생성된 난수를 균등한확률로 배치
+public:
+    //주사위 생성자
+    DiceSystem();
+
+    //주사위를 굴릴때 연출
+    int RollDice();
+    //주사위를 굴리고 결과를 반환하는 함수
+    int ThrowDice();
+};
+
+
